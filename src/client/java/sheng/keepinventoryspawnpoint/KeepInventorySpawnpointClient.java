6e91b36b65b9fbb421f02a771d2d9373eacb4f31
Sairus Seng <sus7211@rit.edu>
2025-06-21 13:18:55 -0400

    Project Template

---
+++ src/client/java/sheng/keepinventoryspawnpoint/KeepInventorySpawnpointClient.java
@@ -0,0 +1,10 @@
+package sheng.keepinventoryspawnpoint;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KeepInventorySpawnpointClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
